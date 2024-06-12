319bca55d613eef251ed575511c44b32b284f93a
clement-delalandre <clement.delalandre@interactiv-group.com>
2024-06-12 08:58:32 +0200

    move images to public

---
+++ src/components/gameHistory.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin">
+      <!--
+        ...content
+        ... use q-card-section for it?
+      -->
+
+      <!-- buttons example -->
+      <q-card-actions align="right">
+        <q-btn color="primary" label="OK" @click="onOKClick" />
+        <q-btn color="primary" label="Cancel" @click="onDialogCancel" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
